fcd74bc908dbd6c5d55a1b26c1136d48b5d66c90
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-19 18:46:28 +0200

    angular/ngStyle

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -9,6 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
+  greenColor = "green";
+  redColor = 'red';
   fruitlist = [
     {
       name: "Apfel",
